355d2603af4911c41eb835dab5a06ee867a69017
JohnEnev <enevoldsen.j@gmail.com>
2025-10-15 17:57:47 +0200

    adding some flags and argv

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.getenv("GEMINI_API_KEY")
+def main():
 
-client = genai.Client(api_key=api_key)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        print("I need a prompt")
+        sys.exit(1)
+    prompt = sys.argv[1]
+
+    verbose=False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose = True
+
+    history = []
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+    
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=prompt
+    )
+
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("No response or usage metadata")
+        return
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Completion tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
